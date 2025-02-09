94eae2fbb820a4eed21659a5a4e7d62f91cbaf75
leumars-o <samex150@gmail.com>
2025-02-09 11:36:08 +0100

    added dexscreener, and rug checker configurations for the script

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ class Config:
     """Set Flask configuration vars from .env file."""
 
     # RPC URL for Solana Mainnet
-    HELIUS_WS_URL: str = os.getenv("HELIUS_WS_URI", "")
-    HELIUS_HTTPS_URL: str = os.getenv("HELIUS_HTTPS_URI_TX", "")
+    HELIUS_WS_URI: str = os.getenv("HELIUS_WS_URI", "")
+    HELIUS_HTTPS_URI_TX: str = os.getenv("HELIUS_HTTPS_URI_TX", "")
+    HELIUS_API_KEY: str = os.getenv("HELIUS_API_KEY", "")
 
     # Telegram Bot Token
     TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
@@ -22,8 +23,8 @@ class Config:
 
     # Raydium Program ID
     LIQUIDITY_POOL = {
-        "raydium_program_id": os.getenv("RAYDIUM_PROGRAM_ID", ""),
-        "wsol_pc_mint": os.getenv("WSOL_PC_MINT", ""),
+        "raydium_program_id": os.getenv("RAYDIUM_PROGRAM_ID", "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"),
+        "wsol_pc_mint": os.getenv("WSOL_PC_MINT", "So11111111111111111111111111111111111111112"),
     }
 
     # Transaction Fetching Config
@@ -35,12 +36,12 @@ class Config:
 
     # Rug Check Config
     RUG_CHECK = {
-        "enabled": os.getenv("RUG_CHECK_ENABLED", "True"),
-        "verbose_log": os.getenv("RUG_CHECK_VERBOSE_LOG", "False"),
+        "enabled": os.getenv("RUG_CHECK_ENABLED", "False"),
+        "verbose_log": os.getenv("RUG_CHECK_VERBOSE_LOG", "True"),
         "allow_mint_authority": os.getenv("RUG_CHECK_ALLOW_MINT_AUTHORITY", "True"),
         "allow_not_initialized": os.getenv("RUG_CHECK_ALLOW_NOT_INITIALIZED", "False"),
         "allow_freeze_authority": os.getenv("RUG_CHECK_ALLOW_FREEZE_AUTHORITY", "True"),
-        "allow_rugged": os.getenv("RUG_CHECK_ALLOW_RUGGED", "False"),
+        "allow_rugged": os.getenv("RUG_CHECK_ALLOW_RUGGED", "True"),
         "allow_mutable": os.getenv("RUG_CHECK_ALLOW_MUTABLE", "True"),
         "block_returning_token_names": os.getenv("RUG_CHECK_BLOCK_RETURNING_TOKEN_NAMES", "True"),
         "block_returning_token_creatores": os.getenv("RUG_CHECK_BLOCK_RETURNING_TOKEN_CREATORS", "True"),
@@ -52,7 +53,7 @@ class Config:
         "min_total_markets": int(os.getenv("RUG_CHECK_MIN_TOTAL_MARKETS", 10)),
         "min_total_lp_providers": int(os.getenv("RUG_CHECK_MIN_TOTAL_LP_PROVIDERS", 5)),
         "min_total_market_liquidity": int(os.getenv("RUG_CHECK_MIN_TOTAL_MARKET_LIQUIDITY", 20000)),
-        "ignore_pump_fun": os.getenv("RUG_CHECK_IGNORE_PUMP_FUN", "True"),
+        "ignore_pump_fun": os.getenv("RUG_CHECK_IGNORE_PUMP_FUN", "False"),
         "max_score": int(os.getenv("RUG_CHECK_MAX_SCORE", 0)), # 0 means no max score
         "legacy_not_allowed": os.getenv("RUG_CHECK_LEGACY_NOT_ALLOWED", ["Copycat token",],)
     }
@@ -60,7 +61,7 @@ class Config:
     # Dexscreener Config
     DEXSCREENER = {
         "chainId": (os.getenv("CHAIN_ID", "solana")),
-        "url": "https://api.dexscreener.com/latest/tokens/",
+        "url": "https://api.dexscreener.com/tokens/v1/",
         "dex_pair_filter": os.getenv("DEX_PAIR_FILTER", "raydium"), # filter for Raydium pairs
         "fetch_max_tries": int(os.getenv("DEXSCREENER_FETCH_MAX_TRIES", 10)),
         "fetch_retry_delay": int(os.getenv("DEXSCREENER_FETCH_RETRY_DELAY", 5)), # in seconds
